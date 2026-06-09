4318e0c859ff84cbf88763a00c2a1464d73b0b5d
heeun98 <phu98@naver.com>
2026-06-09 15:34:41 +0900

    0609

---
+++ src/leetcode/week1/hi.java
@@ -0,0 +1,37 @@
+package leetcode.week1;
+
+import java.util.*;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+
+        List<Integer> list = new ArrayList<>();
+
+
+        int count = 1;
+        list.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i - 1] == nums[i]) {
+                count++;
+
+                if (count <= 2) {
+                    list.add(nums[i]);
+                }
+            } else {
+                list.add(nums[i]);
+                count = 1;
+            }
+
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+
+        return list.size();
+
+
+    }
+}
