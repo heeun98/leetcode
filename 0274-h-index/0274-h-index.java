e1d2a5ba5fd37a564bbb8f158f7e194eec8a7a64
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-06-12 21:27:51 +0900

    Time: 11 ms (5.21%), Space: 43.6 MB (42.16%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int hIndex(int[] citations) {
+
+
+        if (citations.length == 1) {
+            if (citations[0] == 0) {
+                return 0;
+            }
+            return 1;
+        }
+        
+        Arrays.sort(citations);
+
+        int min = Arrays.stream(citations)
+                        .min()
+                        .getAsInt();
+
+        int max = Arrays.stream(citations)
+                        .max()
+                        .getAsInt();
+        int result = 0;
+        int idx = 0;
+        for (int i = 0; i <= max; i++) {
+
+            for (int j = 0; j < citations.length; j++) {
+                if (i <= citations[j]) {
+                    idx = j;
+                    break;
+                }
+            }
+
+            int count = citations.length - idx; // 나보다 큰 숫자의 개수
+            if (i <= count) {
+                result = i;
+            } else {
+                break;
+            }
+
+        }
+
+        return result;
+
+    }
+}
+
+
+// 5,000,000
