766fe4f9a9fe5a38a8b86cbb07817240a596a02d
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-06-28 15:06:48 +0900

    Time: 7 ms (73.17%), Space: 49.9 MB (34.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,50 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
+        Map<String, List<String>> map = new HashMap<>();
 
-        Node[] nodes = new Node[strs.length];
+       for (int i = 0; i < strs.length; i++) {
 
-        for (int i = 0; i < strs.length; i++) {
-            String str = strs[i];
-            char[] ch = str.toCharArray();
+            String tmp = strs[i];
+            char[] ch = strs[i].toCharArray();
+
             Arrays.sort(ch);
-            String newWord = new String(ch);
-            nodes[i] = new Node(i, newWord);
-        }
 
+            String str = new String(ch);
 
-        Arrays.sort(nodes, (o1, o2) -> o1.word.compareTo(o2.word));
-
-        String ss = "";
-
-        // 이 과정 깔끔하게 못하나?
-        List<List<String>> result = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        list.add(strs[nodes[0].idx]);
-
-        for (int i = 1; i < strs.length; i++) {    
-            if (nodes[i].word.equals(nodes[i - 1].word)) {
-                list.add(strs[nodes[i].idx]);
-            } else {
-                result.add(list);
-                list = new ArrayList<>();
-                list.add(strs[nodes[i].idx]);
+            if (!map.containsKey(str)) {
+                map.put(str, new ArrayList<>());
             }
-        }
+            map.get(str).add(tmp);
+       }
 
-        result.add(list);
-
-        return result;
+        return new ArrayList<>(map.values());
 
     }
 
-    static class Node {
-        int idx;
-        String word;
 
-        public Node(int idx, String word) {
-            this.idx = idx;
-            this.word = word;
-        }
-    }
 }
