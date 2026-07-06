8f9df7bcbf2acccdd8bd2e129dea0250a3dcd656
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-07-06 23:25:19 +0900

    [level 3] Title: 단속카메라, Time: 6.94 ms, Memory: 71 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        
+        
+        Arrays.sort(routes, (o1, o2) -> o1[0] - o2[0]);
+        int last = routes[0][1];
+        int cnt = 1;
+        
+        for (int i = 1; i < routes.length; i++) {
+            int[] poll = routes[i];
+            
+            int first = poll[0];
+    
+            if (first <= last) {
+                last = Math.min(last, poll[1]);
+                
+            } else {
+                cnt++;
+                last = poll[1];
+            }
+        }
+        
+        return cnt;
+    }
+}
