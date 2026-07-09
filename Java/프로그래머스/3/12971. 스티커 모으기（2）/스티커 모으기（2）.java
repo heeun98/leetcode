3c3b7e193f56eaf5f33459939589797a71c2cb49
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-07-09 22:15:54 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.04 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public int solution(int sticker[]) {
+
+        int n = sticker.length;
+        int []dp1 = new int [n];
+        int []dp2 = new int [n];
+
+        if(n==1)
+            return sticker[0];
+
+        //첫번째 스티커를 뜯었을 경우
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
+
+        for(int i=2;i<n-1;i++){
+            dp1[i]=Math.max(dp1[i-1],dp1[i-2]+sticker[i]);
+        }
+
+        //첫번째 스티커를 뜯지 않았을 경우
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for(int i=2;i<n;i++){
+            dp2[i]=Math.max(dp2[i-1],dp2[i-2]+sticker[i]);
+        }
+        return Math.max(dp1[n-2],dp2[n-1]);
+    }
+}
