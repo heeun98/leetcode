de2c2e7696ea6dde1a76a50e6b7e5cf6a77b3d71
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-08-05 00:40:10 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 82.8 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,50 @@
+import java.util.*;
+
 class Solution {
-    
-    boolean[] visit;
     public int solution(int n, int[][] computers) {
-        int count = 0;
-        visit = new boolean[n];
+        
+        boolean[] visit = new boolean[n];
+        
+        List<List<Integer>> gr = new ArrayList<>();
         
         for (int i = 0; i < n; i++) {
-            if (visit[i]) continue;
-            dfs(n, computers, i);
-            count++;
+            gr.add(new ArrayList<>());
         }
         
-        return count;
-    }
-    
-    public void dfs(int n, int[][] computers, int idx) {
-        
-        
-        for (int i = 0; i < n; i++) {
-            if (visit[i]) continue;
-            
-            if (computers[idx][i] == 1) {
-                visit[i] = true;
-                dfs(n, computers, i);
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[0].length; j++) {
+                if (j == i) continue;
+                
+                if (computers[i][j] == 1) {
+                    gr.get(i).add(j);
+                    gr.get(j).add(i);
+                }
+                
             }
         }
         
+        
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (visit[i]) continue;
+            visit[i] = true;
+            dfs(i, visit, gr);
+            count++;
+        }
+        return count;
+       
+    }
+    
+    
+    public void dfs(int n, boolean[] visit, List<List<Integer>> gr) {
+        
+        for (int i = 0; i < gr.get(n).size(); i++) {
+            int node = gr.get(n).get(i);
+            if (visit[node]) continue;
+            
+            visit[node] = true;
+            dfs(node, visit, gr);
+        }
     }
 }
