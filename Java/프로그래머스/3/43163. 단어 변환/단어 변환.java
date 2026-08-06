b6f47aa09b28955c5301e9547cc11606f65fbbca
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-08-06 21:18:00 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 80.8 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,63 +1,57 @@
-import java.util.*;
-
 class Solution {
+    
+    boolean[] visit;
+    int result = Integer.MAX_VALUE;
+    boolean flag = false;
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         
-        Queue<String> que = new LinkedList<>();
-        boolean[] visit = new boolean[words.length];
         
-        for (int i = 0; i < words.length; i++) {
-            if (isOneDiff(begin, words[i])) {
-                que.add(words[i]);
-                visit[i] = true;
-            }
+        visit = new boolean[words.length];
+        dfs(0, begin, target, words);
+        
+        if (!flag) {
+            return 0;
+        } else {
+            return result;
         }
-        int dis = 1;
-        boolean flag = false;
-        
-        System.out.println("BFS 전 큐의 상태 : " + que);
-        
-        while (!que.isEmpty()) {
-            for (int i = 0; i < que.size(); i++) {
-                System.out.println("큐 상태 : " + que);
-                String word = que.poll();
-                
-                
-                if (word.equals(target)) {
-                    flag = true;
-                    System.out.println("dis = " + dis);
-                    return dis;
-                }
-                for (int j = 0; j < words.length; j++) {
-                    if (visit[j]) continue; // 이미 방문했으면 최소일수가 없음
-                    if(isOneDiff(word, words[j])) {// 단어차이 1인지
-                        que.add(words[j]);
-                        visit[j] = true;
-                    }
-                }
-            }
-            
-            System.out.println("거리증가! : " + dis);
-            dis++;
-            System.out.println("거리증가 완료 : " + dis);
-            
-        }
-        
-        return 0;
+    
     }
     
-    public boolean isOneDiff(String begin, String compare) {
-        int count = 0;
-        for (int i = 0; i < begin.length(); i++) {
-            if (begin.charAt(i) != compare.charAt(i)) {
-                count++;
-            }
+    
+    public void dfs(int depth, String begin, String target, String[] words) {
+        
+        if (begin.equals(target)) {
+            flag = true;
+            result = Math.min(result, depth);
+            return;
         }
         
-        if (count == 1) return true;
-        
-        return false;
+     
+        for (int i = 0; i < words.length; i++) {
+            if (!canSwitch(begin, words[i])) continue;
+            if (visit[i]) continue;
+            
+            visit[i] = true;
+            dfs(depth + 1, words[i], target, words);
+            visit[i] = false;
+        }
         
     }
+    
+    
+    private boolean canSwitch(String to, String from) {
+        
+        int count = 0;
+        for (int i = 0; i < to.length(); i++) {
+            if ((to.charAt(i) == from.charAt(i))) {
+                continue;
+            }
+            count++;
+            
+            if (count >= 2) return false; 
+        }
+        
+        return true;
+    } 
 }
