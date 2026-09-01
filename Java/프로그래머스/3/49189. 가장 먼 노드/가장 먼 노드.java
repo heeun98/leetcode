39c023e563066838992a276f6d93020af7afada6
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-09-01 18:25:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 17.35 ms, Memory: 111 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,40 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int[][] edge) {
-                
         List<List<Integer>> gr = new ArrayList<>();
         
-        for (int i = 0 ; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             gr.add(new ArrayList<>());
         }
         
         for (int[] e : edge) {
             int from = e[0];
             int to = e[1];
-            
             gr.get(from).add(to);
             gr.get(to).add(from);
         }
         
-        
-        Queue<Integer> que = new LinkedList<>();
-        
         boolean[] visit = new boolean[n + 1];
-        
-        que.add(1);
+        Queue<Integer> que = new LinkedList<>();
         visit[1] = true;
-        int answer = 0;
+        que.offer(1);
         int dis = 0;
+      
+        int count = 0;
+        boolean flag = false;
         
         while (!que.isEmpty()) {
             int size = que.size();
-            if (size > 0) {
-                answer = size;
-            }
-            
+          
             for (int i = 0; i < size; i++) {
                 int now = que.poll();
+                List<Integer> tmp = gr.get(now);
+                flag = false;
                 
-                for (int j = 0; j < gr.get(now).size(); j++) {
-                    
-                    int next = gr.get(now).get(j);
-                    
+                for (int j = 0; j < tmp.size(); j++) {
+                    int next = tmp.get(j);
                     if (visit[next]) continue;
-                    
                     visit[next] = true;
                     que.offer(next);
                 }
@@ -50,10 +42,32 @@ class Solution {
             dis++;
         }
         
-        return answer;
+        que = new LinkedList<>();
+        visit = new boolean[n + 1];
+        visit[1] = true;
+        que.offer(1);
+        int dis1 = 0;
+        count = 0;
         
+        while (!que.isEmpty()) {
+            int size = que.size();
+          
+            for (int i = 0; i < size; i++) {
+                int now = que.poll();
+                if (dis1 == dis - 1) count++;
+                List<Integer> tmp = gr.get(now);
+                flag = false;
+                
+                for (int j = 0; j < tmp.size(); j++) {
+                    int next = tmp.get(j);
+                    if (visit[next]) continue;
+                    visit[next] = true;
+                    que.offer(next);
+                }
+            }
+            dis1++;
+        }
         
+        return count;
     }
-    
-
 }
