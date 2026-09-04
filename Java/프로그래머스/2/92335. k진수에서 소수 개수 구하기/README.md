40358eb87a2f3ecae98ce06987cc44024b6d8549
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-09-04 15:36:33 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 4.67 ms, Memory: 79.3 MB -BaekjoonHub

--- Java/프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ Java/프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 72 MB, 시간: 4.71 ms
+메모리: 79.3 MB, 시간: 4.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 28일 18:58:53
+2026년 09월 04일 15:36:30
 
 ### 문제 설명
 
--- Java/프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ Java/프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,33 +1,28 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = -1;
+
+        String convert = Integer.toString(n, k);
         
-        String result = Integer.toString(n, k);
+        String[] numbers = convert.split("0");
         int count = 0;
-        String[] nums = result.split("0+");
         
-        for (String s : nums) {
-            if (s.isEmpty()) continue;
-            long number = Long.parseLong(s);
+        for (String num : numbers) {
+            if (num.isEmpty()) continue;
+            long num1 = Long.parseLong(num);
             
-            if (isSosu(number)) {
-                count++;
-            }
-            
+            if (isSosu(num1)) count++;
         }
         return count;
     }
     
-    private boolean isSosu(long number) {
-        if (number < 2) return false;
+    private boolean isSosu(long num) {
         
+        if (num == 1 || num == 0) return false;
         
-        for (long i = 2; i * i <= number; i++) {
-            if (number % i == 0) {
-                return false;
-            }
+        for (long i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
         }
-        return true;
         
+        return true;
     }
 }
