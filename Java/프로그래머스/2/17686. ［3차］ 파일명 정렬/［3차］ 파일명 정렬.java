469647b45da6455767120c28e627985b574ac857
heeun98 <132757475+heeun98@users.noreply.github.com>
2026-09-21 17:19:03 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 7.76 ms, Memory: 79.8 MB -BaekjoonHub

--- Java/프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ Java/프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,57 +1,89 @@
 import java.util.*;
 
+
 class Solution {
     public String[] solution(String[] files) {
         
-        File[] fileResult = new File[files.length];
+        
+        File[] result = new File[files.length];
         int idx = 0;
         
-        
-        for (String tmp : files) {
-            File file = new File(tmp);
-            fileResult[idx] = file;
+        for (String name : files) {
+            
+            int numIdx = findIndexFirstNumber(name);
+            // 3
+            
+            String head = name.substring(0, numIdx).toLowerCase();
+                        
+            int lastNumIdx = findIndexEndOfDigit(name.substring(numIdx));
+            // 1
+            String number = name.substring(numIdx, numIdx + lastNumIdx);
+            // 3, 3 + 1
+            String tail = name.substring(numIdx + lastNumIdx);
+            result[idx] = new File(name, head, Integer.parseInt(number), tail);
             idx++;
         }
         
-        // 문자열 비교와, 정수 비교 문법 숙지.
-        // 안정 정렬 숙지. 자바는 언제 안정 정렬을 제공하는지 숙지.
-        Arrays.sort(fileResult, (o1, o2) -> {
+        
+        Arrays.sort(result, (o1, o2) -> {
             if (!o1.head.equals(o2.head)) {
                 return o1.head.compareTo(o2.head);
             }
+        
             return Integer.compare(o1.number, o2.number);
+            
+            
         });
         
         
-        // 객체 배열을 문자열 배여롤 바꾸는 Stream 숙지
-        return Arrays.stream(fileResult)
-            .map(o -> o.s)
+        return Arrays.stream(result)
+            .map(o -> o.origin)
             .toArray(size -> new String[size]);
     }
     
     
-    static class File {
+    public class File {
         
-        String s;
+        String origin;
         String head;
         int number;
+        String tail;
         
-        
-        public File(String s) {
-            this.s = s;
-            
-            int i = 0;
-            
-            while (i < s.length() && !Character.isDigit(s.charAt(i))) i++;
-            
-            int j = i;
-            this.head = s.substring(0, i).toLowerCase(); // 문법 숙지.
-            
-            // i - j < 5 숙지
-            while (i < s.length() && Character.isDigit(s.charAt(i)) && i - j < 5) i++;
-            
-            this.number = Integer.parseInt(s.substring(j, i));
+        public File (String origin, String head, int number, String tail) {
+            this.origin = origin;
+            this.head = head;
+            this.number = number;
+            this.tail = tail;
         }
+    }
+    
+    
+    public int findIndexFirstNumber(String fileName) {
+        
+        char[] sequence = fileName.toCharArray();
+        
+        for (int i = 0; i < sequence.length; i++) {
+            if (!Character.isDigit(sequence[i])) continue;
+            return i;
+        }
+        
+        return fileName.length();
+    }
+    
+    
+    public int findIndexEndOfDigit(String fileName) {
+        
+        
+        char[] sequence = fileName.toCharArray();
+        
+        for (int i = 0; i < sequence.length; i++) {
+            if (Character.isDigit(sequence[i])) continue;
+            return i;
+        }
+        
+        return fileName.length();
+        
+        
         
         
     }
